fa06383f7ff09ad6d4cc48f5255126c79b4e6feb
ulvee <ulvee@mail.ru>
2023-05-30 00:05:41 -0700

    first commit

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication10.DAL
+{
+    public class AppDbContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
